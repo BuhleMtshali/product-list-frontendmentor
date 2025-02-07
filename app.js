f5e672e53a4e1e92e9bb4e62be121b41480e9dac
Buhle <mtshalizanobuhle7@gmail.com>
2025-02-07 20:08:27 +0200

    updated the javascript file with the images

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 //IMPORTING THE DATA
 import  data  from "./assets/data.js";
-console.table(data)
+// console.table(data)
 
-//defining t
-
-
+//defining the variables
+const productContainer = document.getElementById('product-render');
 
 //rendering them out
+const productItems = data.map((product, index) => 
+            `
+                <div class="product" id="${index}">
+                    <img src="${product.image.desktop}" alt="brownie image" id="">
+                    <button class="addBtn">
+                    <i class="fa-solid fa-cart-plus"></i>
+                    Add to Cart
+                    </button>
+                    <small class="catergory">${product.category}</small>
+                    <p class="product-name">${product.name}</p>
+                    <p class="price">$ ${product.price.toFixed(2)}</p>
+                </div>  
+                        `
+).join("")
+
+//check if product exists
+if (productContainer) {
+    productContainer.innerHTML = productItems;
+} else {
+    console.error("Element with ID 'product-render' not found");
+}
+
+//ADDING EVENT LISTNER
