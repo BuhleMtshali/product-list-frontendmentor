1c10e23ee6309e34385c100f6a1c25d728d109e3
Buhle <mtshalizanobuhle7@gmail.com>
2025-02-07 20:39:39 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-//IMPORTING THE DATA
-import  data  from "./assets/data.js";
-// console.table(data)
+    //IMPORTING THE DATA
+    import  data  from "./assets/data.js";
+    // console.table(data)
 
-//defining the variables
-const productContainer = document.getElementById('product-render');
+    //defining the variables
+    const productContainer = document.getElementById('product-render');
 
-//rendering them out
-const productItems = data.map((product, index) => 
-            `
-                <div class="product" id="${index}">
-                    <img src="${product.image.desktop}" alt="brownie image" id="">
-                    <button class="addBtn">
-                    <i class="fa-solid fa-cart-plus"></i>
-                    Add to Cart
-                    </button>
-                    <small class="catergory">${product.category}</small>
-                    <p class="product-name">${product.name}</p>
-                    <p class="price">$ ${product.price.toFixed(2)}</p>
-                </div>  
+    //rendering them out
+    const productItems = data.map((product, index) => {
+            return    `
+                        <div class="product" id="${index}">
+                        <img src="${product.image.desktop}" alt="brownie image" id="">
+                        <button class="addBtn">
+                        <i class="fa-solid fa-cart-plus"></i>
+                        Add to Cart
+                        </button>
+                        <small class="catergory">${product.category}</small>
+                        <p class="product-name">${product.name}</p>
+                        <p class="price">R ${((product.price) * 18.40).toFixed(2)}</p>
+                        </div>  
                         `
-).join("")
+                        handleClick(product); 
+    }).join("")
 
-//check if product exists
-if (productContainer) {
-    productContainer.innerHTML = productItems;
-} else {
-    console.error("Element with ID 'product-render' not found");
-}
+    //checking if container exists
 
-//ADDING EVENT LISTNER
+    if (productContainer) {
+        productContainer.innerHTML = productItems;
+
+        // ADDING AN EVENT LISTENER
+        document.querySelectorAll('.addBtn').forEach((button) => {
+            button.addEventListener('click', () => {
+                console.log('I was clicked')
+            })
+        })
+
+
+        }  else {
+        console.error("Element with ID 'product-render' not found");
+    }
