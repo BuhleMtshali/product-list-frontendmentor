a0e39c142bd36f91b5ba7ae50f121583bcaecc4e
Buhle <mtshalizanobuhle7@gmail.com>
2025-02-08 17:05:12 +0200

    updated all the files

--- app.js
+++ app.js
@@ -1,150 +1,166 @@
-    //IMPORTING THE DATA
-    import  data  from "./assets/data.js";
-    // console.table(data)
+//IMPORTING THE DATA
+import  data  from "./assets/data.js";
+// console.table(data)
 
-    //defining the variables
-    const productContainer = document.getElementById('product-render');
-    const cheoutRender = document.getElementById('checkout-render')
-    const cartContainer = document.getElementById('cart-container')
-    const bottomContainer = document.getElementById('bottom-button-container')
-    const checkoutItemsLength = document.getElementById('items-cart');
-    let finalTotal = document.getElementById('total')
+//defining the variables
+const productContainer = document.getElementById('product-render');
+const cheoutRender = document.getElementById('checkout-render')
+const cartContainer = document.getElementById('cart-container')
+const bottomContainer = document.getElementById('bottom-button-container')
+const checkoutItemsLength = document.getElementById('items-cart');
+let finalTotal = document.getElementById('total')
 
-    const checkoutArray = [];
+const checkoutArray = [];
 
-    //rendering them out
-    const productItems = data.map((product, index) => {
-            return    `
-                        <div class="product" id="${index}">
-                        <img src="${product.image.desktop}" alt="brownie image" id="">
-                        <button class="addBtn" data-index="${index}">
-                        <i class="fa-solid fa-cart-plus cart"></i>
+//rendering them out
+const productItems = data.map((product, index) => {
+        return    `
+                    <div class="product" id="${index}">
+                    <img src="${product.image.desktop}" alt="brownie image" id="">
+                    <button class="addBtn" data-index="${index}">
+                        <i class="fa-solid fa-cart-plus"></i>
                         Add to Cart
-                        </button>
-                        <small class="catergory">${product.category}</small>
-                        <p class="product-name">${product.name}</p>
-                        <p class="price">$ ${product.price.toFixed(2)}</p>
-                        </div>  
-                        `
-        }).join("")
-        
-        //checking if container exists
+                    </button>
+                    <small class="catergory">${product.category}</small>
+                    <p class="product-name">${product.name}</p>
+                    <p class="price">$ ${product.price.toFixed(2)}</p>
+                    </div>  
+                    `
+    }).join("")
+    
+    //checking if container exists
 
-        if (productContainer) {
+    if (productContainer) {
 
-            productContainer.innerHTML = productItems;
+        productContainer.innerHTML = productItems;
 
-                            
+                        
 
-            // ADDING AN EVENT LISTENER to the product card
-            document.querySelectorAll('.addBtn').forEach((button) => {
+        // ADDING AN EVENT LISTENER to the product card
+        document.querySelectorAll('.addBtn').forEach((button) => {
 
             button.addEventListener('click', function(event) { //EVERYTHING BELOW THIS LINE IS TRIGGERED BY THIS BUTTON
-            
-            //increment & decrement buttons 
-            let incrementBtn = document.querySelectorAll('.increment');
-            let decrementBtn = document.querySelectorAll('.decrement');
+
+                    //increment & decrement buttons 
+                    let incrementBtn = document.querySelectorAll('.increment');
+                    let decrementBtn = document.querySelectorAll('.decrement');
+                    
+                    let cards = document.querySelectorAll('.product')
                 
-            let cards = document.querySelectorAll('.product')
-        
-            event.stopPropagation() //stops from triggering parent div click
+                    event.stopPropagation() //stops from triggering parent div click
+                    
+                    const index = this.getAttribute('data-index'); //retrieves the value of the data-index attribute from the element that was clicked
             
-            const index = this.getAttribute('data-index'); //retrieves the value of the data-index attribute from the element that was clicked
-           
-            // checks if the array has the data value
-            if(checkoutArray.some(item => item.id === data[index].id)){
-                    console.log(incrementBtn)
+                // checks if the array has the data value
+                if(checkoutArray.includes(data[index])){
+
+                    decrementBtn.forEach((button) => {
+                        button.addEventListener('click', function() {
+                            console.log("decrement button click")
+                        })
+                    })
+
+
+                    incrementBtn.forEach((btn) => {
+                        btn.addEventListener('click', function() {
+                            console.log("increment btn clicked")
+                        })
+                    })
                    
-
-                } else {
-                    //if it doesn't exist push the to the checkout array
-                    checkoutArray.push(data[index]);
             
-                        if(checkoutArray.length > 0) {
-                        // change the styling of the button if array.length is greater than 0
-                        button.style.backgroundColor = "var(--active-btn-bg)";
-                        button.style.padding = "8px 12px"
-                        button.style.color = "var(--btn-bg)"
-                        button.innerHTML = `<div class="active-icons">
-                                    <i class="fa-solid fa-minus increment"></i>
-                                    <p class="current-quantity">1</p>
-                                    <i class="fa-solid fa-plus decrement"></i>
-                                    </div>
-                                    `
-                        }
-               
-                    //if statement for changing the parent div and calls the function
-                        cards.forEach((item) => {
-                        if(item.id === index){
-                            updateUi(item)
-                    }
+
+             } else {
+                   
+                     //if it doesn't exist push the to the checkout array
+                     checkoutArray.push(data[index]);
+                
+                     if(checkoutArray.length > 0) {
+                     // change the styling of the button if array.length is greater than 0
+                     button.style.backgroundColor = "var(--active-btn-bg)";
+                     button.style.padding = "8px 12px"
+                     button.style.color = "var(--btn-bg)"
+                     button.innerHTML = `
+                                 <div class="active-icons">
+                                 <i class="fa-solid fa-minus increment"></i>
+                                 <p class="current-quantity">1</p>
+                                 <i class="fa-solid fa-plus decrement"></i>
+                                 </div>
+                                `
+                                 
+             }
+                     //if statement for changing the parent div and calls the function
+                     cards.forEach((item) => {
+                     if(item.id === index){
+                         updateUi(item)
+
+
+                }
+    })
+        //function for displaying the checkout array
+        updateCheckoutDisplay()
+
+        // function to calculate the price
+        calculatePrice()
+
+       
+                }        
         })
-            //function for displaying the checkout array
-            updateCheckoutDisplay()
 
-            // function to calculate the price
-            calculatePrice()
+    })
 
-           
-                    }        
-            })
-
-        })
-
-        }  else {
-            console.error("Element with ID 'product-render' not found");
-        }
-            
-        //updating the ui of the old array if an item is added to the cart
-        function updateUi(product) {
-                // console.log(product)
-                product.style.border = "2px solid var(--active-btn-bg)"
-            
-            }
-
-        //rendering the items in the cart and updating the UI
-        function updateCheckoutDisplay(){
+    }  else {
+        console.error("Element with ID 'product-render' not found");
+    }
         
-        bottomContainer.style.display = 'block' //if something is added to the array the checkout btn must appear
-
-        checkoutItemsLength.textContent = checkoutArray.length //updating the length of the array
-
-        console.log(checkoutArray)
-
-        if(cheoutRender){
-
-                cheoutRender.innerHTML = checkoutArray.map((item, index) => {
-
-                  return `
-                    <div class="checkoutcard">
-                        <p class="product-name">${item.name}</p>
-                        <div class="checkout-info">
-                        <div class="left">
-                        <p class="quantity">1x</p>
-                        <p class="checkout-price">
-                        <span class="price-each">@ $${item.price}</span>
-                        <span class="total-each">$${item.price}</span>
-                        </p>
-                        </div>
-                        <i class="fa-regular fa-circle-xmark"></i>
-                        </div>
-                </div> 
-                        `
-                }).join("")
-            }
+    //updating the ui of the old array if an item is added to the cart
+    function updateUi(product) {
+            // console.log(product)
+            product.style.border = "2px solid var(--active-btn-bg)"
+        
         }
 
-
+    //rendering the items in the cart and updating the UI
+    function updateCheckoutDisplay(){
     
-        //function to calculate total price
-        function calculatePrice(){
+    bottomContainer.style.display = 'block' //if something is added to the array the checkout btn must appear
 
-            const totalPrice = checkoutArray.reduce((total, item) => {
-                return total + item.price;
-            }, 0)
+    checkoutItemsLength.textContent = checkoutArray.length //updating the length of the array
 
-            console.log(totalPrice)
-            finalTotal.textContent = `$${totalPrice}`
+    console.log(checkoutArray)
+
+    if(cheoutRender){
+
+            cheoutRender.innerHTML = checkoutArray.map((item, index) => {
+
+              return `
+                <div class="checkoutcard">
+                    <p class="product-name">${item.name}</p>
+                    <div class="checkout-info">
+                    <div class="left">
+                    <p class="quantity">1x</p>
+                    <p class="checkout-price">
+                    <span class="price-each">@ $${item.price}</span>
+                    <span class="total-each">$${item.price}</span>
+                    </p>
+                    </div>
+                    <i class="fa-regular fa-circle-xmark"></i>
+                    </div>
+            </div> 
+                   ` 
+            }).join("")
         }
+    }
 
-    
+
+
+    //function to calculate total price
+    function calculatePrice(){
+
+        const totalPrice = checkoutArray.reduce((total, item) => {
+            return total + item.price;
+        }, 0)
+
+        console.log(totalPrice)
+        finalTotal.textContent = `$${totalPrice}`
+    }
+
